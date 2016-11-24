ab7e027eed136104fb0ea697a121fd894125a08c
Christopher Wallis <supercryptonite@gmail.com>
2016-11-24 11:40:06 -0500

    setup test with ts-node and program to run with ts-node

---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { blue } from 'typestyle/lib/csx';
+
+console.log(`The pen is ${blue}`);
---
+++ tests/main.ts
@@ -0,0 +1,7 @@
+import * as assert from 'assert';
+
+describe('main', () => {
+    it('works!', () => {
+        assert(true);
+    });
+});
